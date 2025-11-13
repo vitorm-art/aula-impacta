9d24685be1523a5444d0ec41aab0106da0e8a6f5
Vitor <vitormoraes0308@gmail.com>
2025-11-13 19:06:29 -0300

    Javascript

---
+++ js/introducao.js
@@ -0,0 +1,23 @@
+// alert('Hello World!!!')
+console.log('Hello world!!!');
+
+var nome;
+var valorCurso = 550.42;
+
+nome = 'Impacta';
+
+
+console.log(valorCurso + 10.00);
+console.log(valorCurso - 10.00);
+console.log(valorCurso * 10.00);
+console.log(valorCurso / 10.00);
+
+var idade = 10;
+var soma;
+
+soma= idade++;
+
+
+
+console.log (soma)
+
