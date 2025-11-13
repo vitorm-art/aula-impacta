6fb2e07924cfd8e1d42d3ff1e72a55f710e3baad
Vitor <vitormoraes0308@gmail.com>
2025-11-13 20:59:23 -0300

    Excercicio1 JS

--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,6 +1,6 @@
 var nomeProduto = prompt('digite o nome do produto') 
 var quantidadeProduto = prompt ('digite a quantidade')
-var valorProduto = 10
+var valorProduto = prompt (10)
 var nomeCliente = prompt ('digite seu nome')
 
 console.log (nomeCliente)
@@ -9,7 +9,7 @@ console.log (valorProduto)
 console.log(valorProduto * quantidadeProduto);
 
 var volteSempre = prompt ('Obrigado e volte sempre!')
-console.log('Obrigado e volte sempre!')
+console.log('Obrigado e volte sempre!Sabão')
 
 
 
