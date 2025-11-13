c661a9a0bbd8fbadcc83fcbd6a313a145cddb65e
Vitor <vitormoraes0308@gmail.com>
2025-11-13 20:57:03 -0300

    Excercicio1 JS

---
+++ js/exercicio1.js
@@ -0,0 +1,16 @@
+var nomeProduto = prompt('digite o nome do produto') 
+var quantidadeProduto = prompt ('digite a quantidade')
+var valorProduto = 10
+var nomeCliente = prompt ('digite seu nome')
+
+console.log (nomeCliente)
+console.log (nomeProduto)
+console.log (valorProduto)
+console.log(valorProduto * quantidadeProduto);
+
+var volteSempre = prompt ('Obrigado e volte sempre!')
+console.log('Obrigado e volte sempre!')
+
+
+
+
--- js/introducao.js
+++ js/introducao.js
@@ -7,17 +7,65 @@ var valorCurso = 550.42;
 nome = 'Impacta';
 
 
-console.log(valorCurso + 10.00);
-console.log(valorCurso - 10.00);
-console.log(valorCurso * 10.00);
-console.log(valorCurso / 10.00);
+// console.log(valorCurso + 10.00);
+// console.log(valorCurso - 10.00);
+// console.log(valorCurso * 10.00);
+// console.log(valorCurso / 10.00);
 
 var idade = 10;
 var soma;
 
-soma= idade++;
+soma= --idade;
 
 
 
-console.log (soma)
+// console.log (soma)
+
+/* Contacatenação e Interpolação */
+var firstName = "Camila";
+var lastName = "da Silva";
+
+var nomeCompletoC = firstName + ' ' + lastName;
+var nomeCompletoI = `Nome: ${firstName} ${lastName}`;
+// console.log(nomeCompletoI)
+
+/*Operadores relacionais */
+// Operators: ==,===. !=, !==, >=, >, <, <=
+// + Operador unario
+var val1 = +"10";
+var val2 = 10;
+var comparar = val1 >= val2;
+// console.log(comparar);
+
+/* Truthy e Falsy */
+var contrato = "";
+
+// console.log(contrato)
+
+/* Estrutura de seleção */
+// var notaAluno = 8
+// var nomeAluno = "Paulo Enrique"
+// var faltas = 10
+
+// // se o aluno tiver uma nota >= 7 aprovado e faltas <= 5
+// // se menor que 5 é reprovado
+// // se estiver >= 5 recuperação
+
+// if(notaAluno >= 7 && !(faltas <= 5)) {
+//     console.log("Aprovado")
+// } 
+// else if(notaAluno >=5 %% faltas <= 5)
+//     console.log ('Recuperação')
+
+// else {
+//     console.log('Reprovado')
+// }
+
+// var resultado
+
+// resultado = notaAluno >=7 ? 'Aprovado' : 'Reprovado'
+// console.log(resultado)
+
+ var nomeCLiente = prompt ('digite seu nome')
+
 
